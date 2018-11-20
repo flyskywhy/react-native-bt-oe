1158f743d992834a5938abc2392bb5124f2b2b0c
Li Zheng <flyskywhy@gmail.com>
2018-11-20 16:42:54 +0800

    fix(设备分组): telink 的组地址是从 0x8001 而非 0x0000 开始的; 可同时设置 groupAddress 的个数取较小的 telink 的值 8

--- index.native.js
+++ index.native.js
@@ -7,6 +7,8 @@ const NativeModule = NativeModules.OeBt;
 class OeBt {
     static MESH_ADDRESS_MIN = 0x8001;
     static MESH_ADDRESS_MAX = 0x8FFF;
+    static GROUP_ADDRESS_MIN = 0x0000;
+    static GROUP_ADDRESS_MAX = 0x7FFF;
     static HUE_MIN = 0;
     static HUE_MAX = 360;
     static SATURATION_MIN = 0;
